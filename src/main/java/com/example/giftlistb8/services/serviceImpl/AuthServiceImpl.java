046aa9bbefd012232d8189b32d85ce54dae40654
Nuradil <nuradil936@gmail.com>
2023-04-14 17:35:27 +0600

    add validations email and password

--- src/main/java/com/example/giftlistb8/services/serviceImpl/AuthServiceImpl.java
+++ src/main/java/com/example/giftlistb8/services/serviceImpl/AuthServiceImpl.java
@@ -7,19 +7,23 @@ import com.example.giftlistb8.dto.auth.responses.AuthRegisterResponse;
 import com.example.giftlistb8.entities.User;
 import com.example.giftlistb8.enums.Role;
 import com.example.giftlistb8.exceptions.AlreadyExistsException;
+import com.example.giftlistb8.exceptions.BadRequestException;
 import com.example.giftlistb8.repositories.UserRepository;
 import com.example.giftlistb8.services.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
@@ -53,15 +57,20 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public AuthRegisterResponse authenticate(AuthAuthenticateRequest userRegisterRequest) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        userRegisterRequest.email(),
-                        userRegisterRequest.password()
-                )
-        );
         User user = userRepository.findByEmail(userRegisterRequest.email())
-                .orElseThrow(() -> new NoSuchElementException(String.format
-                        ("User with email: %s doesn't exists", userRegisterRequest.email())));
+                .orElseThrow(() ->
+                        new BadRequestException("Invalid email or password."));
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            userRegisterRequest.email(),
+                            userRegisterRequest.password()
+                    )
+            );
+        } catch (AuthenticationException e) {
+            log.error("Invalid email or password");
+            throw new BadCredentialsException("Invalid email or password.");
+        }
         String token = jwtService.generateToken(user);
 
         return AuthRegisterResponse.builder()
