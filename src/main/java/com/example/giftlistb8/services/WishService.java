af9561d60da040b44009a19422eab47d0ff66328
nurislamnurkanov <nurislamnurkanov@gmail.com>
2023-04-19 11:12:30 +0600

    all delete comments

--- src/main/java/com/example/giftlistb8/services/WishService.java
+++ src/main/java/com/example/giftlistb8/services/WishService.java
@@ -6,11 +6,6 @@ import com.example.giftlistb8.dto.wish.responses.WishResponce;
 import java.util.List;
 
 public interface WishService {
-//    RestaurantResponse getById(Long id);
-//    SimpleResponse save(RestaurantRequest request);
-//    List<RestaurantAllResponse> findAll();
-//    SimpleResponse delete(Long id);
-//    SimpleResponse update(Long id,RestaurantRequest request);
     WishResponce getById(Long id);
 
 }
