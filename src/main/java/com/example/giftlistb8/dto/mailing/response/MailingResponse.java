6022918b56781b43bc0892460e2f68156d690b72
Nurbek <amanbekovnurbek04@gmail.com>
2023-05-01 09:23:57 +0600

    crud_my_friends

---
+++ src/main/java/com/example/giftlistb8/dto/mailing/response/MailingResponse.java
@@ -0,0 +1,11 @@
+package com.example.giftlistb8.dto.mailing.response;
+
+import java.time.LocalDateTime;
+
+public class MailingResponse {
+    private Long id;
+    private String name;
+    private String image;
+    private String text;
+    private LocalDateTime createdAt;
+}
