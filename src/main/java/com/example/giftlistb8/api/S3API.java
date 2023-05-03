7255b82700d30c8e43dda8218a3105655c152ce9
Nuradil <nuradil936@gmail.com>
2023-05-03 20:08:42 +0600

    add swagger tag and operation

--- src/main/java/com/example/giftlistb8/api/S3API.java
+++ src/main/java/com/example/giftlistb8/api/S3API.java
@@ -2,6 +2,8 @@ package com.example.giftlistb8.api;
 
 import com.example.giftlistb8.config.s3.S3Service;
 import com.example.giftlistb8.dto.s3.response.S3Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,12 +13,14 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/file")
+@CrossOrigin(origins = "*")
+@Tag(name = "S3", description = "API for working with files in Amazon S3 storage")
 public class S3API {
-
 
     private final S3Service s3Service;
 
-
+    @Operation(summary = "Upload a file to S3 bucket",
+            description = "Uploads a file to the specified S3 bucket.")
     @PostMapping("/upload")
     public S3Response uploadFile(@RequestParam("file") MultipartFile file) {
         try {
@@ -26,6 +30,8 @@ public class S3API {
         }
     }
 
+    @Operation(summary = "Delete a file from S3 bucket",
+            description = "Deletes a file from the specified S3 bucket.")
     @DeleteMapping("/delete/{key}")
     public S3Response deleteFile(@PathVariable String key) {
         return s3Service.deleteFile(key);
