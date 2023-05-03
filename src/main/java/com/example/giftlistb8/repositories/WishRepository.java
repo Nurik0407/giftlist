5a87e27697117e109c23fc94e1064d817d72e141
Bektursun Adylbek uulu <bektursun.adylbekuulu@gmail.com>
2023-05-03 11:37:01 +0600

    first commit create complaints

--- src/main/java/com/example/giftlistb8/repositories/WishRepository.java
+++ src/main/java/com/example/giftlistb8/repositories/WishRepository.java
@@ -4,13 +4,11 @@ import com.example.giftlistb8.dto.wish.responses.WishResponse;
 import com.example.giftlistb8.entities.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface WishRepository extends JpaRepository<Wish,Long> {
+public interface WishRepository extends JpaRepository<Wish, Long> {
     @Query("select new com.example.giftlistb8.dto.wish.responses.WishResponse(w.id,w.name,w.image,w.status) from Wish w where w.id=:id")
     Optional<WishResponse> findWishById(Long id);
 
