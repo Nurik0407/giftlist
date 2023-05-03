417bb029f81f4c395387945832b57c6d2a331d53
Bektursun Adylbek uulu <bektursun.adylbekuulu@gmail.com>
2023-05-03 21:51:11 +0600

    conflict resolved

--- src/main/java/com/example/giftlistb8/repositories/custom/impl/ComplaintRepositoryCustomImpl.java
+++ src/main/java/com/example/giftlistb8/repositories/custom/impl/ComplaintRepositoryCustomImpl.java
@@ -19,7 +19,6 @@ public class ComplaintRepositoryCustomImpl implements ComplaintRepositoryCustom 
     public ComplaintResponse getAllComplaints() {
 
         String sql = """
-<<<<<<< HEAD
                 SELECT CONCAT(u.first_name, ' ', u.last_name) AS full_name,
                        ui.image AS user_image,
                        ch.name AS charity_name,
@@ -54,37 +53,9 @@ public class ComplaintRepositoryCustomImpl implements ComplaintRepositoryCustom 
                         JOIN user_infos ui ON u.user_info_id = ui.id
                         JOIN wishes_complaints cc on wh.id = cc.wish_id
                         JOIN complaints c on cc.complaints_id = c.id;
-=======
-                SELECT CONCAT(u.first_name, ' ', u.last_name) AS full_name, 
-                ui.image AS user_image,
-                ch.name AS charity_name,
-                ch.date_of_issue AS charity_date_of_issue, 
-                ch.image AS charity_image,
-                c.complaint AS complaint_text,
-                ui2.image AS complaint_user_image
-                FROM charities ch
-                         JOIN users u ON ch.user_id = u.id
-                         JOIN user_infos ui ON u.user_info_id = ui.id
-                         JOIN complaints c ON u.id = c.user_id
-                         JOIN users u2 ON c.user_id = u2.id
-                         JOIN user_infos ui2 ON u2.user_info_id = ui2.id;
+          
                 """;
-        String sql2 = """
-                SELECT CONCAT(u.first_name, ' ', u.last_name) AS full_name,
-                ui.image AS user_image,
-                w.name AS wish_name,
-                w.date_of_holiday AS wish_holiday_date,
-                w.image AS wish_image,
-                c.complaint AS complaint_text,
-                ui2.image AS complaint_user_image
-                FROM wishes w
-                         JOIN users u ON w.user_id = u.id
-                         JOIN user_infos ui ON u.user_info_id = ui.id
-                         JOIN complaints c ON u.id = c.user_id
-                         JOIN users u2 ON c.user_id = u2.id
-                         JOIN user_infos ui2 ON u2.user_info_id = ui2.id
->>>>>>> 4a23aad (first commit create complaints)
-                """;
+
         ComplaintResponse complaintResponse = new ComplaintResponse();
 
         List<CharityResponseWIthComplaint> charityResponses = jdbcTemplate.query(sql, (resultSet, i) ->
@@ -94,13 +65,8 @@ public class ComplaintRepositoryCustomImpl implements ComplaintRepositoryCustom 
                         resultSet.getString("charity_name"),
                         resultSet.getDate("charity_date_of_issue"),
                         resultSet.getString("charity_image"),
-<<<<<<< HEAD
                         resultSet.getString("complaint_user_image"),
                         resultSet.getString("complaint_text")
-=======
-                        resultSet.getString("complaint_text"),
-                        resultSet.getString("complaint_user_image")
->>>>>>> 4a23aad (first commit create complaints)
                 ));
 
         complaintResponse.setCharityResponseWIthComplaints(charityResponses);
@@ -111,13 +77,8 @@ public class ComplaintRepositoryCustomImpl implements ComplaintRepositoryCustom 
                         resultSet.getString("wish_name"),
                         resultSet.getDate("wish_holiday_date"),
                         resultSet.getString("wish_image"),
-<<<<<<< HEAD
                         resultSet.getString("complaint_user_image"),
                         resultSet.getString("complaint_text")
-=======
-                        resultSet.getString("complaint_text"),
-                        resultSet.getString("complaint_user_image")
->>>>>>> 4a23aad (first commit create complaints)
                 ));
         complaintResponse.setWishResponseWithComplaints(wishResponses);
 
