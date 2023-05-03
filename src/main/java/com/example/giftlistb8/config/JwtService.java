b982f6efc7da5ddf91f31dda40b16ea70ea30df0
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-05-03 11:42:46 +0600

    import security context

--- src/main/java/com/example/giftlistb8/config/JwtService.java
+++ src/main/java/com/example/giftlistb8/config/JwtService.java
@@ -12,6 +12,7 @@ import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
