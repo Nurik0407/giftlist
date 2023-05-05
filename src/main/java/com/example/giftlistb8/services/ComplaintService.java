3df5b9283ef8b94f93d51e12dba9906c2d55289d
Bektursun Adylbek uulu <bektursun.adylbekuulu@gmail.com>
2023-05-05 11:52:42 +0600

    Conflict resolved WishRepository

--- src/main/java/com/example/giftlistb8/services/ComplaintService.java
+++ src/main/java/com/example/giftlistb8/services/ComplaintService.java
@@ -1,8 +1,12 @@
 package com.example.giftlistb8.services;
 
 import com.example.giftlistb8.dto.SimpleResponse;
+import com.example.giftlistb8.dto.complaint.response.ComplaintResponse;
 
 public interface ComplaintService {
 
     SimpleResponse complaintToCharity(Long id);
+    SimpleResponse complaintToWish(Long id);
+
+    ComplaintResponse getAll();
 }
