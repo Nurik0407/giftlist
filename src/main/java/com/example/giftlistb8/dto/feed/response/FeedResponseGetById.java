a268c372776314a7e60a5488a8c2997cb834d44d
ZHIIDEGUL <emranlini@gmail.com>
2023-05-05 12:49:11 +0600

    Added forgot field of feed

---
+++ src/main/java/com/example/giftlistb8/dto/feed/response/FeedResponseGetById.java
@@ -0,0 +1,19 @@
+package com.example.giftlistb8.dto.feed.response;
+
+import lombok.Builder;
+
+import java.time.LocalDate;
+@Builder
+public record FeedResponseGetById(
+        Long userId,
+        String image,
+        String fullName,
+        String holidayName,
+        String wishName,
+        String description,
+        String photo,
+        LocalDate date,
+        Boolean status,
+        String friendPhoto
+) {
+}
