3ecf9fc43d2f8c4589fba8b4ec31a8d531e4bb53
Bektursun Adylbek uulu <bektursun.adylbekuulu@gmail.com>
2023-05-10 18:18:12 +0600

    fourth commit with Git rebase

--- src/main/java/com/example/giftlistb8/services/ComplaintService.java
+++ src/main/java/com/example/giftlistb8/services/ComplaintService.java
@@ -3,6 +3,7 @@ package com.example.giftlistb8.services;
 import com.example.giftlistb8.dto.SimpleResponse;
 import com.example.giftlistb8.dto.complaint.response.ComplaintResponse;
 
+
 public interface ComplaintService {
 
     SimpleResponse complaintToCharity(Long id);
