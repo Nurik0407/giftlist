94d1ba41f069d8f7919bcc152d61abfbdc2a0fff
Bektursun Adylbek uulu <bektursun.adylbekuulu@gmail.com>
2023-05-12 03:17:10 +0600

    conflict resolved with git rebase

--- src/main/java/com/example/giftlistb8/services/serviceImpl/CharityAdminServiceImpl.java
+++ src/main/java/com/example/giftlistb8/services/serviceImpl/CharityAdminServiceImpl.java
@@ -1,22 +1,34 @@
 package com.example.giftlistb8.services.serviceImpl;
 
+import com.example.giftlistb8.config.JwtService;
+import com.example.giftlistb8.dto.SimpleResponse;
+import com.example.giftlistb8.dto.charity.request.CharityRequest;
+import com.example.giftlistb8.dto.charity.request.CharityUpdateRequest;
 import com.example.giftlistb8.dto.charity.response.CharitiesResponse;
+import com.example.giftlistb8.dto.charity.response.CharityResponse;
+import com.example.giftlistb8.entities.Charity;
+import com.example.giftlistb8.entities.User;
+import com.example.giftlistb8.exceptions.NotFoundException;
 import com.example.giftlistb8.repositories.CharityRepository;
 import com.example.giftlistb8.services.CharityAdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CharityAdminServiceImpl implements CharityAdminService {
     private final JdbcTemplate jdbcTemplate;
+    private final CharityRepository charityRepository;
+    private final JwtService jwtService;
 
     @Override
     public List<CharitiesResponse> findAll() {
-        String sql = "SELECT u.id as user_id,CONCAT(u.last_name, ',', u.first_name) AS full_name, ui.image,"+
+        String sql = "SELECT u.id as user_id,CONCAT(u.last_name, ',', u.first_name) AS full_name, ui.image," +
                 "c.id,c.name, c.image, c.date_of_issue,c.state,case when r.id = null then false else true end  AS is_reserved, COALESCE(r.is_anonymous, false) AS is_anonymous " +
                 "FROM charities c " +
                 "JOIN users u ON c.user_id = u.id " +
@@ -33,5 +45,56 @@ public class CharityAdminServiceImpl implements CharityAdminService {
                 resultSet.getString("state"),
                 resultSet.getBoolean("is_reserved"),
                 resultSet.getBoolean("is_anonymous")));
+    }
+
+    @Override
+    public SimpleResponse save(CharityRequest request) {
+        User userInToken = jwtService.getUserInToken();
+        Charity charity = Charity.builder()
+                .name(request.name())
+                .state(request.state())
+                .description(request.description())
+                .category(request.category())
+                .subCategory(request.subCategory())
+                .image(request.image())
+                .dateOfIssue(LocalDate.now())
+                .user(userInToken)
+                .build();
+        charityRepository.save(charity);
+        return SimpleResponse.builder()
+                .status(HttpStatus.OK)
+                .message(String.format("Charity with name %s successfully saved.", charity.getName()))
+                .build();
+    }
+
+    @Override
+    public SimpleResponse update(CharityUpdateRequest request) {
+        jdbcTemplate.update("update charities set name=?,state=?,description=?,category=?,sub_category=?,image=? WHERE id=?",
+                request.name(), request.state(), request.description(), request.category(), request.subCategory(), request.image(), request.id());
+        return SimpleResponse.builder()
+                .status(HttpStatus.OK)
+                .message(String.format("Charity with id %s successfully updated.", request.id()))
+                .build();
+    }
+
+    @Override
+    public CharityResponse findById(Long id) {
+        return charityRepository.findCharityById(id)
+                .orElseThrow(() -> new NotFoundException(
+                        String.format("Charity with id %s not found.", id)));
+    }
+
+    @Override
+    public SimpleResponse delete(Long id) {
+        User userInToken = jwtService.getUserInToken();
+        Charity charity = charityRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(
+                        String.format("Charity with id %s not found.", id)));
+        userInToken.deleteCharity(charity);
+        charityRepository.deleteById(id);
+        return SimpleResponse.builder()
+                .status(HttpStatus.OK)
+                .message(String.format("Charity with id %s successfully deleted.", id))
+                .build();
     }
 }
