1fc2c05ccc9cd26bc171771ce20f6c4c72fb0e81
Bektursun Adylbek uulu <bektursun.adylbekuulu@gmail.com>
2023-05-12 03:17:10 +0600

    conflict resolved with git rebase

--- src/main/java/com/example/giftlistb8/api/CharityAdminAPI.java
+++ src/main/java/com/example/giftlistb8/api/CharityAdminAPI.java
@@ -1,11 +1,14 @@
 package com.example.giftlistb8.api;
 
+import com.example.giftlistb8.dto.SimpleResponse;
+import com.example.giftlistb8.dto.charity.request.CharityRequest;
+import com.example.giftlistb8.dto.charity.request.CharityUpdateRequest;
 import com.example.giftlistb8.dto.charity.response.CharitiesResponse;
+import com.example.giftlistb8.dto.charity.response.CharityResponse;
 import com.example.giftlistb8.services.CharityAdminService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +21,25 @@ public class CharityAdminAPI {
     @GetMapping
     public List<CharitiesResponse> findALl(){
         return service.findAll();
+    }
+
+    @PostMapping
+    public SimpleResponse save(@RequestBody @Valid CharityRequest request) {
+        return service.save(request);
+    }
+
+    @PutMapping
+    public SimpleResponse update(@RequestBody @Valid CharityUpdateRequest request) {
+        return service.update(request);
+    }
+
+    @GetMapping("/profile")
+    public CharityResponse profile(@RequestParam Long id) {
+        return service.findById(id);
+    }
+    @DeleteMapping
+    public SimpleResponse delete(@RequestParam Long id){
+        return service.delete(id);
     }
 
 }
