fb52de47cd4975abbd24a8aacfa9f2949df92d9b
nurislamnurkanov <nurislamnurkanov@gmail.com>
2023-06-02 12:38:56 +0600

    j unit test wishlist

--- src/main/java/com/example/giftlistb8/repositories/WishRepository.java
+++ src/main/java/com/example/giftlistb8/repositories/WishRepository.java
@@ -4,10 +4,12 @@ import com.example.giftlistb8.dto.wish.responses.WishResponse;
 import com.example.giftlistb8.entities.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
     @Query("select new com.example.giftlistb8.dto.wish.responses.WishResponse(w.id,w.name,w.image,w.status) from Wish w where w.id=:id and w.isBlocked=false ")
     Optional<WishResponse> findWishById(Long id);
