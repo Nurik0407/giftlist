43d8b5b55f1eae582519dd8a8e6945de02abba0f
Djoldoshov <nuradil936@gmail.com>
2023-06-23 18:59:29 +0600

    data-init change

--- src/main/java/com/example/giftlistb8/services/serviceImpl/CharityServiceImpl.java
+++ src/main/java/com/example/giftlistb8/services/serviceImpl/CharityServiceImpl.java
@@ -75,7 +75,10 @@ public class CharityServiceImpl implements CharityService {
 
     @Override
     public List<CharitiesResponse> findAll() {
-        String sql = "SELECT u.id as user_id,CONCAT(u.last_name, ',', u.first_name) AS full_name,ui.image,c.id,c.name, c.image, c.date_of_issue,c.state, (case when r.id is null then false else true end) as is_reserved, COALESCE(r.is_anonymous, false) AS is_anonymous " +
+        String sql = "SELECT u.id as user_id,CONCAT(u.last_name, ' ', u.first_name) AS full_name," +
+                "ui.image as userImage,c.id,c.name as image, c.image, c.date_of_issue,c.state," +
+                " (case when r.id is null then false else true end) as is_reserved," +
+                " COALESCE(r.is_anonymous, false) AS is_anonymous " +
                 "FROM charities c " +
                 "JOIN users u ON c.user_id = u.id " +
                 "LEFT JOIN user_infos ui ON u.user_info_id = ui.id " +
@@ -83,7 +86,7 @@ public class CharityServiceImpl implements CharityService {
         List<CharitiesResponse> charitiesResponses = jdbcTemplate.query(sql, (rs, rowNum) -> new CharitiesResponse(
                 rs.getLong("user_id"),
                 rs.getString("full_name"),
-                rs.getString("image"),
+                rs.getString("userImage"),
                 rs.getLong("id"),
                 rs.getString("name"),
                 rs.getString("image"),
