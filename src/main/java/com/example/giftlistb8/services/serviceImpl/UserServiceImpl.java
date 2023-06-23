af88406da9266cc92ad01961e6ab389a24904443
Djoldoshov <nuradil936@gmail.com>
2023-06-23 20:29:57 +0600

    change user api getById

--- src/main/java/com/example/giftlistb8/services/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/giftlistb8/services/serviceImpl/UserServiceImpl.java
@@ -20,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
 
 
 @Service
